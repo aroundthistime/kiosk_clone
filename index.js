bda22e1dc6e78e4af0413b6e2841c97d4227908c
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2020-11-15 23:58:58 +0900

    modified stylesheet name error
    changed mistyping the title of stylesheet href (styles -> style)

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import path from 'path';
+
 const app = express();
 
 const PORT = 5000;
 
-const handleListening = () =>
-  console.log(`✅ Listening on https://localhost/${PORT}`);
+const handleListening = () => console.log(`✅ Listening on https://localhost/${PORT}`);
 
-let db = [];
+const db = [];
 let number = 1;
-let a = [2, 3];
+const a = [2, 3];
 app.listen(PORT, handleListening);
 app.set('view engine', 'pug');
-app.set("views", path.join(__dirname, "views"));
-app.use("/static", express.static(path.join(__dirname, "static")));
-app.get("/", (req, res) => res.render("index"));
-app.get("/order", (req, res) => res.render("order"));
-app.get("/alert", (req, res) => res.render("alert"));
-app.post("/api", (req, res) => {
+app.set('views', path.join(__dirname, 'views'));
+app.use('/build', express.static(path.join(__dirname, 'build')));
+app.get('/', (req, res) => res.render('index'));
+app.get('/order', (req, res) => res.render('order'));
+app.get('/alert', (req, res) => res.render('alert'));
+app.post('/api', (req, res) => {
   db.push(number);
   res.json(number);
   number += 1;
   res.end();
 }); // just an example, in the real case you don't need axios to send orders.
 
-app.get("/api", (req, res) => {
+app.get('/api', (req, res) => {
   res.json(db);
   res.end();
-})
+});
 
-app.get("/api/alert", (req, res) => {
+app.get('/api/alert', (req, res) => {
   res.json(db);
   res.end();
-})
+});
 
-app.get("/api/alert/:id", (req, res) => {
+app.get('/api/alert/:id', (req, res) => {
   res.json(db);
   res.end();
-})
+});
 
-app.post("/api/alert/:id", (req, res) => {
+app.post('/api/alert/:id', (req, res) => {
   const {
-    params : {id}
+    params: { id },
   } = req;
   const index = db.indexOf(id);
-  if (index > -1){
+  if (index > -1) {
     db.splice(index, 1);
   }
   res.end();
-})
+});
