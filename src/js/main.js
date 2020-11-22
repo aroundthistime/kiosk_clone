f502cf770e6134d0c7cd89a19c9c80d62de4a4e7
KG LEE <acrkdduf@naver.com>
2020-11-23 01:40:18 +0900

    매출관리 디자인 완성

--- src/js/main.js
+++ src/js/main.js
@@ -16,4 +16,34 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
   calendar.render();
+
+  const chartEl = document.getElementById('myChart').getContext('2d');
+  const myChart = new Chart(chartEl, {
+    type: 'bar',
+    data: {
+      labels: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+      datasets: [{
+          label: '단위(만원) ',
+          data: [120, 190, 30, 50, 20, 30, 20, 20 ,30, 40, 80],
+          backgroundColor: [
+            'rgba(36, 102, 250, 0.2)', 'rgba(36, 12, 50, 0.2)', 'rgba(3, 102, 20, 0.2)', 'rgba(36, 182, 50, 0.2)',
+            'rgba(136, 202, 200, 0.2)', 'rgba(36, 182, 50, 0.2)', 'rgba(36, 202, 150, 0.2)', 'rgba(60, 12, 250, 0.2)',
+            'rgba(36, 122, 150, 0.2)', 'rgba(236, 152, 250, 0.2)', 'rgba(136, 152, 250, 0.2)', 'rgba(160, 12, 50, 0.2)',
+          ],
+          borderWidth: 2
+      }]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+      }
+    }
+  }); 
 });
