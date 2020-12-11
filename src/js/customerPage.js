af8e6e522694c1692a18f1edce94908961b2d204
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2020-12-12 02:47:42 +0900

    고객주문페이지 메뉴정보 팝업 수정
    고객주문 페이지에서 메뉴팝업이 자동적으로 맨 위로 스크롤되지 않아 엉뚱한 지점을 기점으로 팝업이 켜지는 현상 발생 -> html상으로 .menuDetails__top추가한 후 scrollIntoView()활용하여 menuDetails팝업이 보여질 때 자동으로 맨 위로 스크롤되도록함

--- src/js/customerPage.js
+++ src/js/customerPage.js
@@ -167,6 +167,7 @@ const fillMenuDetailsPopup = (isCombo) => {
     sideOption.classList.add(INVISIBLE);
   }
   showPopup(menuDetailsPopup);
+  menuDetailsPopup.querySelector('.menuDetails__top').scrollIntoView(); // menuDetails팝업의 맨 위로 스크롤
 };
 
 const isBurger = (menu) => {
@@ -678,7 +679,7 @@ const initCustomerPage = () => {
   prepareForHereToGoPopup();
   prepareCustomerFinalBtns(); // 최종 주문, 최종 취소 버튼
   document.getElementById('orderResultBtn').addEventListener('click', refreshCustomerPage); // 주문 다 끝나고 확인버튼 누르면 refresh
-  detectMouseMove(); //페이지 초기화된 상태에서도 3분동안 아무런 조작이 없으면 갱신(할인 등의 정보 항상 최신화 유지)
+  detectMouseMove(); // 페이지 초기화된 상태에서도 3분동안 아무런 조작이 없으면 갱신(할인 등의 정보 항상 최신화 유지)
   window.addEventListener('mousemove', detectMouseMove);
 };
 
